8a0e55f1c346715270852bc8bb63c1e5f22c02f1
moon <moonbae77@gmail.com>
2016-04-10 19:02:06 -0700

    application

---
+++ src/main/java/pos/application/MainApplication.java
@@ -0,0 +1,13 @@
+package pos.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+	
+}
